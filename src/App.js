c262e4488c6ead68f5b6a18c15eacae94201627b
Dervla O’Brien <dervla.obrien@method4.co.uk>
2019-06-21 15:51:00 +0100

    Added random adjectives

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
 
+function shuffleAdjectives(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function ARandomJakeBerg(){
+  const adjectiveList = ["large", "big", "smiley", "[s t u p i d]", "red", "orange", "yellow", "green", "blue", "purple", "pink", "glittery", "teeny", "* t i n y *", "frowny", "excellent", "bad"];
+  const chosenAdjective = adjectiveList[Math.floor(Math.random() * quotes.length)];
+  <JakeBerg adjective={chosenAdjective}></JakeBerg>
+}
 
 function JakeBerg(props){
   return (
-    <div>I have a {props.adjective} face</div>
+    <div>
+      <h1>My name is Ja Kob Mountain</h1>
+      <p>I have a {props.adjective} face</p>
+    </div>
   );
 }
 
@@ -15,7 +34,7 @@ function App() {
         <p>
           HELLO WORLD
         </p>
-        <JakeBerg adjective="Large"></JakeBerg>
+        <ARandomJakeBerg></ARandomJakeBerg>
       </header>
     </div>
   );
